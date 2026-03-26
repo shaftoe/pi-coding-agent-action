095d287b3130034d657478ce9837b5300257a79b
Alexander Fortin <shaftoe@users.noreply.github.com>
2026-03-26 08:11:57 -0400

    wip

---
+++ src/github.ts
@@ -0,0 +1,73 @@
+import * as core from '@actions/core';
+import * as github from '@actions/github';
+import RestEndpointMethodTypes from '@octokit/plugin-rest-endpoint-methods';
+
+type createReactionType =
+  RestEndpointMethodTypes.RestEndpointMethodTypes['reactions']['createForIssueComment']['response'];
+type deleteReactionType =
+  RestEndpointMethodTypes.RestEndpointMethodTypes['reactions']['deleteForIssueComment']['response'];
+type createCommentType =
+  RestEndpointMethodTypes.RestEndpointMethodTypes['issues']['createComment']['response'];
+
+const trigger = core.getInput('trigger') || '/pi';
+const octokit = github.getOctokit(core.getInput('github_token'));
+
+export async function addReaction(
+  comment: typeof github.context.payload.comment
+): Promise<createReactionType | undefined> {
+  if (!comment) {
+    core.notice('no comment found, skipping reaction');
+    return;
+  }
+
+  return await octokit.rest.reactions.createForIssueComment({
+    owner: github.context.repo.owner,
+    repo: github.context.repo.repo,
+    comment_id: comment.id,
+    content: 'eyes',
+  });
+}
+
+export async function getComment(): Promise<typeof github.context.payload.comment | undefined> {
+  const comment = github.context.payload.comment;
+  if (!comment) {
+    core.notice('no comment found in context, skipping prompt');
+    return;
+  }
+
+  comment.body = comment.body.replace(trigger, '').trim();
+
+  return comment;
+}
+
+export async function deleteReaction(
+  reaction: createReactionType | undefined,
+  comment: typeof github.context.payload.comment
+): Promise<deleteReactionType | undefined> {
+  if (!reaction || !comment) {
+    return;
+  }
+
+  return octokit.rest.reactions.deleteForIssueComment({
+    owner: github.context.repo.owner,
+    repo: github.context.repo.repo,
+    comment_id: comment.id,
+    reaction_id: reaction.data.id,
+  });
+}
+
+export async function createComment(
+  comment: typeof github.context.payload.comment,
+  body: string
+): Promise<createCommentType | undefined> {
+  if (!comment || !body) {
+    return;
+  }
+
+  return octokit.rest.issues.createComment({
+    owner: github.context.repo.owner,
+    repo: github.context.repo.repo,
+    issue_number: comment.issue_number,
+    body,
+  });
+}
--- src/run.ts
+++ src/run.ts
@@ -1,43 +1,38 @@
 import * as core from '@actions/core';
-import * as github from '@actions/github';
 import { Client } from './pi';
+import { getComment, addReaction, deleteReaction, createComment } from './github';
 
 const provider = core.getInput('provider');
 const model = core.getInput('model');
 const token = core.getInput('token');
-const githubToken = core.getInput('github_token');
 const thinkingInput = core.getInput('thinking_level') ?? 'off';
-const trigger = core.getInput('trigger') || '/pi';
 
 export async function run() {
-  const comment = github.context.payload.comment;
+  const comment = await getComment();
   if (!comment) {
     core.notice('no comment found in context, skipping prompt');
     return;
   }
-  const prompt = comment.body.replace(trigger, '').trim();
-  const pi = await new Client(model, provider, token, thinkingInput).ready();
 
-  const octokit = github.getOctokit(githubToken);
-  const reaction = await octokit.rest.reactions.createForIssueComment({
-    owner: github.context.repo.owner,
-    repo: github.context.repo.repo,
-    comment_id: comment.id,
-    content: 'eyes',
-  });
+  const prompt = comment.body;
+  if (!prompt) {
+    core.notice('no prompt found in comment, skipping prompt');
+    return;
+  }
 
-  const result = await pi.prompt(prompt);
+  // add eyes reaction to the comment, will be removed before final comment
+  const reaction = await addReaction(comment);
+  let result: string;
 
-  await octokit.rest.reactions.deleteForIssueComment({
-    owner: github.context.repo.owner,
-    repo: github.context.repo.repo,
-    comment_id: comment.id,
-    reaction_id: reaction.data.id,
-  });
-  await octokit.rest.issues.createComment({
-    owner: github.context.repo.owner,
-    repo: github.context.repo.repo,
-    issue_number: github.context.issue.number,
-    body: result,
-  });
+  try {
+    const pi = await new Client(model, provider, token, thinkingInput).ready();
+    result = await pi.prompt(prompt);
+  } catch (e) {
+    await deleteReaction(reaction, comment);
+    await createComment(comment, e instanceof Error ? e.message : String(e));
+    throw e;
+  }
+
+  await deleteReaction(reaction, comment);
+  await createComment(comment, result);
 }
